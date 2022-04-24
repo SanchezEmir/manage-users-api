19b2e1c4c229c39f9419519e60c5e0131d7ee440
Sanchez Emir <emir2104@gmail.com>
2022-04-24 11:32:23 -0500

    initial commit

---
+++ src/main/java/com/emirsanchez/usersmanage/UsersmanageApplication.java
@@ -0,0 +1,13 @@
+package com.emirsanchez.usersmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersmanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emirsanchez/usersmanage/UsersmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emirsanchez.usersmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
